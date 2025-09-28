d08416102f0a103b0752cfc91946e1996ca61350
vik_mlops <vikmlops@gmail.com>
2025-09-28 20:00:41 +0530

    name-change-AB#3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
 
-    <h1>Welcome to Vikash MLOPs world</h1>
-	<h2>Upload your data to get Gen Ai predictions MLOPs </h2>
+    <h1>Welcome to Vikash MLOPs world-Manual_app_Stage_Prod</h1>
+	<h2>Upload your data to get Gen Ai predictions MLOPs demo of Manual Approval Staging Production </h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
